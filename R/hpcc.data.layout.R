af1259a74a7c1ac113b226ce85c52be844bc9776
unknown <703063698@703063698L.IND.CORP.AD>
2014-11-13 23:21:11 +0530

    Updated Begin:   R/hpcc.begin.R 	updated data layout:   R/hpcc.data.layout.R 	updated dataset:   R/hpcc.dataset.R 	updated execute:   R/hpcc.execute.R 	updated output:   R/hpcc.output.R 	updated download file:   R/hpcc.showFilesToDownload.R 	updated package:   package/rhpcc_1.0.tar.gz

--- R/hpcc.data.layout.R
+++ R/hpcc.data.layout.R
@@ -1,45 +1,56 @@
 hpcc.data.layout <- function(logicalfilename) {
-		uUrl <- hpcc.get.url()
-		out.struct <- ""
-		body <- paste('<?xml version="1.0" encoding="utf-8"?>
-					  <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
-					  xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
-					  xmlns="urn:hpccsystems:ws:wsdfu">
-					  <soap:Body>
-					  <DFUInfoRequest>
-					  <Name>',logicalfilename,'</Name>
-					  </DFUInfoRequest>
-					  </soap:Body>
-					  </soap:Envelope>')
+	#		uUrl <- .hpcc.get.url()
+	out.struct <- ""
+	body <- paste('<?xml version="1.0" encoding="utf-8"?>
+				  <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
+				  xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
+				  xmlns="urn:hpccsystems:ws:wsdfu">
+				  <soap:Body>
+				  <DFUInfoRequest>
+				  <Name>',logicalfilename,'</Name>
+				  </DFUInfoRequest>
+				  </soap:Body>
+				  </soap:Envelope>')
+	
+	headerFields =
+		c(Accept = "text/xml",
+		  Accept = "multipart/*",
+		  'Content-Type' = "text/xml; charset=utf-8",
+		  SOAPAction="urn:hpccsystems:ws:wsdfu")
+	
+	reader = basicTextGatherer()
+	
+	handle = getCurlHandle()
+#	url <- 'https://216.19.105.2:18010/EclDirect'
+	url <- .uUrlHpcc
+	# 	proxy<-readline(prompt="Do you use a proxy connection(Y/N) : ")
+	# 	if(proxy=='Y'||proxy=='y') {}
+# 	opts <- list(
+# 		proxy         = "", 
+# 		proxyusername = "", 
+# 		proxypassword = "", 
+# 		proxyport     = ""
+# 	)
+	
+	curlPerform(url = url,
+				httpheader = headerFields,
+				ssl.verifypeer = FALSE,
+				postfields = body,
+				writefunction = reader$update,
+				curl = handle)
+	
+	status = getCurlInfo(handle)$response.code
+	if(status >= 200 && status <= 300) {
+		sResponse <- reader$value()
+		responseXml <- xmlParse(sResponse)
 		
-		headerFields =
-			c(Accept = "text/xml",
-			  Accept = "multipart/*",
-			  'Content-Type' = "text/xml; charset=utf-8",
-			  SOAPAction="urn:hpccsystems:ws:wsdfu")
-		
-		reader = basicTextGatherer()
-		
-		handle = getCurlHandle()
-		curlPerform(url = uUrl,
-					httpheader = headerFields,
-					postfields = body,
-					writefunction = reader$update,
-					curl = handle
-		)
-		
-		status = getCurlInfo( handle )$response.code
-		if(status >= 200 && status <= 300) {
-			sResponse <- reader$value()
-			responseXml <- xmlParse(sResponse)
-			
-			layout <- getNodeSet(responseXml, "//*[local-name()='Ecl']/text()", 
-								 namespaces = xmlNamespaceDefinitions(responseXml,simplify = TRUE))
-			if(length(layout) > 0) {
-				colLayout <- layout[[1]]
-				out.struct <- xmlToList(colLayout,addAttributes=TRUE)
-			}
+		layout <- getNodeSet(responseXml, "//*[local-name()='Ecl']/text()", 
+							 namespaces = xmlNamespaceDefinitions(responseXml,simplify = TRUE))
+		if(length(layout) > 0) {
+			colLayout <- layout[[1]]
+			out.struct <- xmlToList(colLayout,addAttributes=TRUE)
 		}
-		
-		out.struct
+	}
+	
+	return(out.struct)
 }
--- R/hpcc.showFilesToDownload.R
+++ R/hpcc.showFilesToDownload.R
@@ -1,52 +1,87 @@
-hpcc.formURL <- function(fileName) {
-	url <- paste("http://",hpccHostName,":",hpccPort,"/FileSpray/DownloadFile?Name=",fileName,"&NetAddress=",hpccHostName,"&Path=/var/lib/HPCCSystems/mydropzone/&OS=1",sep='')
+.hpcc.formURL <- function(fileName) {
+	url <- paste("http://",.hpccHostName,":",.hpccPort,"/FileSpray/DownloadFile?Name=",fileName,"&NetAddress=",.hpccHostName,"&Path=/var/lib/HPCCSystems/mydropzone/&OS=1",sep='')
 	return(url)
 }
 
 
+
 hpcc.showFilesToDownload <- function() {
-	if(length(hpccSessionVariables)<2) {
+	if(dim(.hpccSessionVariables)[1]==0) {
 		print("No Files To download")
 		return()
 	}
 	print('Below are the files available for download')
-	for(i in seq(1,length(hpccSessionVariables),by=2)) {
-		print(paste(hpccSessionVariables[i],'--',hpccSessionVariables[(i+1)],sep="  "))
-		i<-i+1
+	for(i in seq(1,dim(.hpccSessionVariables)[1])) {
+# 		print(paste(as.numeric(.hpccSessionVariables[i,1]),'--',.hpccSessionVariables[i,2],sep="  "))
+		print(.hpccSessionVariables[i,2])
+		
 	}
-	a <- TRUE
-	while(a) {
+	inp <- 1
+	while(inp!=0) {
 		inp <- readline(prompt="Type the number of the file To Download or 0 to exit : ")
 		inp <- as.numeric(inp)
-		if(inp>0 & inp<=length(hpccSessionVariables)/2) {
+		if(inp>0 & inp<=dim(.hpccSessionVariables)[1]) {
 			numb <- as.numeric(inp)
-			if(hpccSessionVariables[numb,2] %in% hpccDownloadedFiles[,2])
+			if(.hpccSessionVariables[numb,3]>0)
 				print('File already downloaded:')
 			else {
-				nameOfFile <- paste(hpccSessionVariables[numb,2],'.csv',sep='')
-				url <- hpcc.formURL(nameOfFile)
+				nameOfFile <- paste(.hpccSessionVariables[numb,2],'.csv',sep='')
+				url <- .hpcc.formURL(nameOfFile)
 				print(url)
-				hpcc.downloadFile(url,nameOfFile)
-				rbind(hpccDownloadedFiles,c((length(hpccDownloadedFiles)/2)+1,hpccSessionVariables[numb,2]))
+				.hpcc.downloadFile(url,nameOfFile)
+				numberOfDown <- sum(as.numeric(.hpccSessionVariables[,3]>0))+1 
+				.hpccSessionVariables[numb,3] <- numberOfDown
 			}
-			
 		}
-		else if(inp==0) {
-			a <- FALSE
-		}
-		else
+		else if(inp!=0)
 			print('Invalid Input')
 	}
 }
 
 
 
-hpcc.downloadFile <- function(url,fileName) {
+hpcc.convertFilesToFFObjects <- function() {
+	if(sum(as.numeric(.hpccSessionVariables[,3]))==0) {
+		print("No Files are downloaded in your system")
+		return()
+	}
+	print('Below are the files downloaded into your System ')
+	
+	downloadedFiles <- .hpccSessionVariables(.hpccSessionVariables[,3]>0)
+	for(i in seq(1,length(downloadedFiles))) {
+		print(paste(downloadedFiles[i,3],'--',downloadedFiles[i,2],sep="  "))
+	}
+	inp <- 1
+	while(inp>0) {
+		inp <- readline(prompt="Type the file number indicated above to convert that file or 0 to exit : ")
+		inp <- as.numeric(inp)
+		if(inp>0 & inp<=dim(downloadedFiles)[1]) {
+			numb <- as.numeric(inp)
+			if(.hpccSessionVariables[numb,3]==1)
+				print('File already Converted:')
+			else {
+				nameOfFile <- paste(.hpccSessionVariables[numb,2],'.csv',sep='')
+				url <- .hpcc.formURL(nameOfFile)
+				print(url)
+				.hpcc.downloadFile(url,nameOfFile)
+				.hpccSessionVariables[numb,4]=1
+			}
+		}
+		else if(inp!=0) {
+			print('Invalid Input')
+		}
+	}
+}
+
+
+
+.hpcc.downloadFile <- function(url,fileName) {
 	ds <- rawToChar(as.raw(92))
 	ds <- sprintf('%s%s',ds,ds)
 	sd <- gsub('/',ds,getwd())
 	print(sd)
-	dest <- paste(sd,rawToChar(as.raw(92)),'rhpcc',rawToChar(as.raw(92)),'downloads',rawToChar(as.raw(92)),'fileName',sep='')
+	dest <- paste(sd,rawToChar(as.raw(92)),'rhpcc',rawToChar(as.raw(92)),
+				  'downloads',rawToChar(as.raw(92)),fileName,sep='')
 	download.file(url,dest)
 	print(paste('File Downloaded at ',dest))
 }
